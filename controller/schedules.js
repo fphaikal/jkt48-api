b87f2e63a98967f62866dde9d25dee775cbf0874
Fahreza Pasha Haikal <abufahreza@gmail.com>
2023-09-30 18:24:22 +0700

    Update schedules.js

--- controller/schedules.js
+++ controller/schedules.js
@@ -4,42 +4,38 @@ const cheerio = require("cheerio")
 const schedule = {
   getSchedule: async (req, res) => {
     const response = await axios.get("https://jkt48.com/calendar/list?lang=id");
-
     const $ = cheerio.load(response.data);
-
+  
     const scheduleData = {
-      period: $(
-        ".entry-schedule__header .entry-schedule__header--center"
-      ).text(),
+      period: $(".entry-schedule__header .entry-schedule__header--center").text(),
       listSchedule: [],
     };
-
+  
     $(".entry-schedule__calendar .table tbody tr").each((i, element) => {
       const schedule = $(element);
-
       const date = schedule.find("td h3").text();
-      const event = schedule
-        .find("td div.contents")
-        .toArray()
-        .map((eventElement) => {
-          const title = $(eventElement).parent().find("p a").text();
-          const category = $(eventElement)
-            .parent()
-            .find("span img")
-            .attr("src");
-
-          return {
-            title,
-            category,
-          };
+      
+      // Mencari semua elemen event
+      const eventElements = schedule.find("td div.contents");
+      const events = [];
+  
+      // Memproses setiap elemen event
+      eventElements.each((j, eventElement) => {
+        const title = $(eventElement).find("p a").text().trim();
+        const category = $(eventElement).find("span img").attr("src");
+  
+        events.push({
+          title,
+          category,
         });
-
+      });
+  
       scheduleData.listSchedule.push({
         date,
-        event,
+        event: events,
       });
     });
-
+  
     return res.status(200).json(scheduleData);
   },
 
