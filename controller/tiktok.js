f061372e9a754615a1904e1014201589994a6750
Fahreza Pasha Haikal <abufahreza@gmail.com>
2024-03-06 18:23:49 +0700

    update tiktok scrapper

--- controller/tiktok.js
+++ controller/tiktok.js
@@ -1,15 +1,23 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 const puppeteer = require("puppeteer");
+const fs = require('fs');
+
+const tiktokAccounts = [
+  "jkt48.erine_",
+  "jkt48.indira.s",
+  "marsha.jkt48",
+];
 const Tiktok = {
-  getData: async (req, res) => {
-    const { member } = req.params;
+  getData: async (username) => {
     // Mulai browser Chromium
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
 
     // Buka halaman TikTok yang ingin di-scrap
     const page = await browser.newPage();
-    await page.goto(`https://www.tiktok.com/${member}`);
+    await page.goto(`https://www.tiktok.com/@${username}`);
 
     // Tunggu hingga data dimuat
     await page.waitForSelector('.css-1g04lal-DivShareLayoutHeader-StyledDivShareLayoutHeaderV2');
@@ -27,12 +35,28 @@ const Tiktok = {
       scrapedData.likes = userInfo.querySelector('[data-e2e="likes-count"]').textContent.trim();
       return scrapedData;
     });
-      
 
-    console.log(data);
+    // Tutup browser setelah selesai
+    await browser.close();
 
-    return res.status(200).json({ data });
+    // Simpan data ke file JSON
+    fs.writeFile(`../db/tiktok/${username}_data.json`, JSON.stringify(data), (err) => {
+      if (err) {
+        console.error(`Error writing to JSON file for ${username}:`, err);
+        return;
+      }
+      console.log(`Data saved to ${username}_data.json`);
+    });
   },
 };
+
+// Panggil fungsi getData untuk setiap akun TikTok pada interval yang ditentukan
+tiktokAccounts.forEach(username => {
+  Tiktok.getData(username);
+  // Setiap 5 jam, data akan diperbarui
+  setInterval(() => {
+    Tiktok.getData(username);
+  }, 5 * 60 * 60 * 1000);
+});
 
 module.exports = Tiktok;
--- routes/tiktokRoute.js
+++ routes/tiktokRoute.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const tiktok = require('../controller/tiktok');
 const router = express.Router();
+const fs = require('fs');
 
-router.get("/:member", tiktok.getData);
 
-
-module.exports = router;
+router.get("/:member", (req, res) => {
+    const { member } = req.params;
+    const filePath = `./db/tiktok/${member}_data.json`;
+  
+    // Membaca data dari file JSON
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Error reading file:', err);
+        return res.status(500).json({ error: 'Failed to read data' });
+      }
+      const jsonData = JSON.parse(data);
+      res.status(200).json(jsonData);
+    });
+  });
+  
+  module.exports = router;
